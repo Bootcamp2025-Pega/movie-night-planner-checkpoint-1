1265904660f573e4a018e940c353c155aad5f8ba
Marek Jagielski <marek.jagielski@pm.me>
2025-02-10 16:18:57 +0100

    init fronted app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Container maxWidth="md" style={{ marginTop: '2rem' }}>
+      <Typography variant="h4" gutterBottom>
+        Movie Night Planner
+      </Typography>
+      {/* Your components will go here */}
+    </Container>
+  );
+}
+
+export default App;
